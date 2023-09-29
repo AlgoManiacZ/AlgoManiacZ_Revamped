354e1270aeee4a5318df431fb6526d2a855d7665
durgaprasad118 <durgaprasadachana1108@gmail.com>
2023-09-29 06:41:51 +0530

    Contact page

--- algoManiacZ/src/App.jsx
+++ algoManiacZ/src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from './components/Hero/HeroSection'
 import Navbar from './components/Navbar/NavBar'
 import SpeakerSection from './components/Speakers/SpeakerSection'
+import Contact from './components/Contact/Contact'
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <HeroSection></HeroSection>
-      <SpeakerSection></SpeakerSection>
+      {/* <HeroSection></HeroSection> */}
+      {/* <SpeakerSection></SpeakerSection> */}
+      <Contact></Contact>
     </>
   )
 }
---
+++ algoManiacZ/src/components/Contact/Todo.md
@@ -0,0 +1,16 @@
+- row for larger screens
+- for row (h-calc)
+
+- for smaller devices
+
+  - col
+  - col (h -auto)
+
+- Left
+
+  - join us (1/2 for lg)
+  - for smaller (full width)
+
+- right side
+  - form (2/3) for large screen
+  - for smaller (full width)

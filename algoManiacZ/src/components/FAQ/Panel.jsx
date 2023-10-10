7cd9dcbc136ef0a7b5fcda6fc92633b6f69af3d0
karthik nadar <karthiknadar1204@gmail.com>
2023-10-10 22:16:43 +0530

    Revert "navbar and faq display issues"

--- algoManiacZ/src/components/FAQ/Panel.jsx
+++ algoManiacZ/src/components/FAQ/Panel.jsx
@@ -5,14 +5,12 @@ const Panel = ({ title, children, isActive, onShow }) => {
   return (
     <div
       className={`bg-orange ${
-        isActive ? 'h-fit' : 'h-30'
+        isActive ? 'h-fit' : 'h-20'
       }  rounded-xl py-4 px-3 duration-200 transition-h ease-in-out flex flex-col justify-center`}
     >
       <div
         className="flex justify-between items-center px-5 gap-x-3"
         onClick={onShow}
-        
-
       >
         <h3 className="md:text-xl text-md font-bold">{title}</h3>
         <BiPlus className="text-3xl"></BiPlus>

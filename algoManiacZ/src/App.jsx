c3c138fa0d7a5bbb4f684fe11e5d5f92e74b66bf
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2023-10-15 22:06:13 +0530

    refactor: Updated folder and component names

--- algoManiacZ/src/App.jsx
+++ algoManiacZ/src/App.jsx
@@ -5,11 +5,11 @@ import Contact from "./components/Contact/Contact";
 import Faq from "./components/FAQ/Faq";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Layout from "./components/ScrollToTop/scrollToTop"
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop"
 
 function App() {
   return (
-    <Layout>
+    <ScrollToTop>
       <Navbar />
       <Home />
       <HeroSection />
@@ -17,7 +17,7 @@ function App() {
       <Faq />
       <Contact />
       <Footer />
-    </Layout>
+    </ScrollToTop>
   );
 }
 
